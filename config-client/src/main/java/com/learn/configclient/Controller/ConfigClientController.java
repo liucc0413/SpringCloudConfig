71649dcfd1c26b8875323609e24ccebfa12be866
l0683 <liucc@dtdream.com>
2019-01-25 18:39:57 +0800

    first commit

---
+++ config-client/src/main/java/com/learn/configclient/Controller/ConfigClientController.java
@@ -0,0 +1,16 @@
+package com.learn.configclient.Controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/client")
+public class ConfigClientController {
+    @Value("${foo}")
+	String foo;
+	@RequestMapping(value = "/hi")
+	public String hi(){
+		return foo;
+	}
+}
