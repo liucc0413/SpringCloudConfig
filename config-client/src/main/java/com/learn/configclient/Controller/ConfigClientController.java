55433778239b86cccca6cf32307fccfb29d12ef2
l0683 <liucc@dtdream.com>
2019-01-30 17:14:20 +0800

    config 自动刷新和秘钥加密

--- config-client/src/main/java/com/learn/configclient/Controller/ConfigClientController.java
+++ config-client/src/main/java/com/learn/configclient/Controller/ConfigClientController.java
@@ -1,11 +1,13 @@
 package com.learn.configclient.Controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/client")
+@RefreshScope
 public class ConfigClientController {
     @Value("${foo}")
 	String foo;
